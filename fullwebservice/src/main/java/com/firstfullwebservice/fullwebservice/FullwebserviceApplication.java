3ca75de28c159ddce3b3ab45f23c1987b7a1eaaf
Kalyan Pamarthy <kalyan@Kalyans-MacBook-Pro.local>
2018-01-01 20:26:17 -0800

    initial commit

---
+++ fullwebservice/src/main/java/com/firstfullwebservice/fullwebservice/FullwebserviceApplication.java
@@ -0,0 +1,12 @@
+package com.firstfullwebservice.fullwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullwebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullwebserviceApplication.class, args);
+	}
+}
